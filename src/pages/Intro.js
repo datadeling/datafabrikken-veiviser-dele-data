0981e10c9bb03e692eedfe4c635db623f7ed5dd9
shenluna <luna.shen@testify.no>
2022-06-28 12:06:16 +0200

    chore: update intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -21,17 +21,28 @@ function Intro({ close, data }) {
         <Primitives.Wizard>
             <IntroMain>
                 <Primitives.Heading.H1>Kan du dele datasettet?</Primitives.Heading.H1>
-                <Primitives.Paragraphs.Lead>Ønsker du å dele data, men er usikker på om du har lov til å dele data? Gå gjennom veilederen for å finne ut av om du lovlig kan dele, eller om det er noe mer du må gjøre eller finne ut av først.</Primitives.Paragraphs.Lead>
+                <Primitives.Paragraphs.Lead>Ønsker du å dele data, men er usikker på om du har lov til å dele data? Vår Veileder hjelper deg med å finne ut om du lovlig kan dele data, eller hva du eventuelt må endre for å kunne dele.</Primitives.Paragraphs.Lead>
+                <Primitives.Heading.H2>Hva inneholder veilederen</Primitives.Heading.H2>
                 <Primitives.Paragraphs.P>
-                Svarene kan du ta vare på slik at du kan huske hva du må finne ut av og kan dokumentere vurderingene du har foretatt. Hvis du har behov for å finne ut av ting kan du komme tilbake for å fortsette veilederen senere.
+                Veilederen guider deg gjennom noen sentrale juridiske spørsmål du må besvare før du deler data.
+                </Primitives.Paragraphs.P>
+                <Primitives.Paragraphs.P>
+                Målgruppen for denne Veilederen er både jurister og ikke-jurister som ønsker svar på spørsmålet “kan jeg dele disse dataene?”. Veiledningen tilpasser seg svarene som oppgis og vurderingene som foretas. I enkelte tilfeller gis det også forklaringer og eksempler som hjelper deg videre om du har behov for mer informasjon.
+                </Primitives.Paragraphs.P>
+                <Primitives.Paragraphs.P>
+                Veiledningen er <u><b>ikke</b></u> uttømmende. Det betyr at den kun hjelper deg med å svare på noen sentrale juridiske spørsmål. Det kan være andre spørsmål som ikke besvares her. Du er selv ansvarlig for å sørge for at din deling av data skjer i henhold til gjeldende regelverk.
+                </Primitives.Paragraphs.P>
+                <Primitives.Heading.H2>Start nå og fullfør senere</Primitives.Heading.H2>
+                <Primitives.Paragraphs.P>
+                Når du er ferdig med Veilederen, kan du laste ned en PDF med resultatene dine. Resultatene gir deg en oversikt over hva du må finne ut av og kan blant annet benyttes som grunnlag for å dokumentere vurderingene som er foretatt. Det er mulig å ta en pause i veilederen ved behov, og komme tilbake for å fortsette senere.
                 </Primitives.Paragraphs.P>
                 <Primitives.Heading.H2>Før du begynner</Primitives.Heading.H2>
                 <Primitives.Paragraphs.P>
-                Før du begynner er det lurt å finne fram til dokumentasjon som inneholder informasjon om dataene du ønsker å dele. Dette er typisk:
+                Det er lurt å finne fram til dokumentasjon som inneholder informasjon om dataene du ønsker å dele. Dette er typisk:
                 </Primitives.Paragraphs.P>
                 <ul>
-                    <li>Internkontroll for virksomhetens behandling av personopplysninger</li>
-                    <li>Behandlingsprotokoll</li>
+                    <li><a href="https://www.datatilsynet.no/rettigheter-og-plikter/virksomhetenes-plikter/informasjonssikkerhet-internkontroll/etablere-internkontroll/">Internkontroll</a> for virksomhetens behandling av personopplysninger</li>
+                    <li><a href="https://www.datatilsynet.no/rettigheter-og-plikter/virksomhetenes-plikter/protokoll-over-behandlingsaktiviteter/">Behandlingsprotokoll</a></li>
                     <li>Avtaler, lisenser, og annet som regulerer datasettene som ønskes delt</li>
                 </ul>
                 <Primitives.Paragraphs.P>
@@ -44,9 +55,22 @@ function Intro({ close, data }) {
                 </ul>
                 <Primitives.Paragraphs.P>Dette kan hjelpe deg å svare riktig i veilederen. </Primitives.Paragraphs.P>
                 <br />
-                <Primitives.Heading.H2>Start nå og fullfør senere</Primitives.Heading.H2>
+                <Primitives.Heading.H2>Ansvar</Primitives.Heading.H2>
                 <Primitives.Paragraphs.P>
-                Det er du som er ansvarlig for at alle lover og regler blir fulgt når du deler data. Er det noen spørsmål du er usikker på underveis? Da kan du stille spørsmål i Datalandsbyen eller ta kontakt med våre juridiske rådgivere.
+                Du er selv ansvarlig for at alle lover og regler blir fulgt når du deler data. Vår veileder fungerer som et hjelpeverktøy som lar deg navigere hvilke vurderinger som skal foretas, samt når og hvordan. Datafabrikken arbeider for at Veilederen er oppdatert og nøyaktig.
+                </Primitives.Paragraphs.P>
+                <Primitives.Paragraphs.P>
+                Brukeren aksepterer og er innforstått med at bruk av Veilederen kun er et teknisk eller elektronisk hjelpemiddel som tilbys slik det til enhver tid er. Enhver bruk av Veilederen skjer på Brukerens egen regning og risiko.
+                </Primitives.Paragraphs.P>
+                <Primitives.Paragraphs.P>
+                Datafabrikken gir ingen garantier for tjenestens nøyaktighet eller bruksområde mv og fraskriver seg ethvert direkte og indirekte ansvar for feil og mangler, samt ansvar for skade og/eller tap av noen art som følge av bruk av Veilederen.
+                </Primitives.Paragraphs.P>
+                <Primitives.Paragraphs.P>
+                Veilederen er fremdeles under utvikling. Dersom du oppdager feil, unøyaktigheter eller mangler, ber vi om at du melder inn dette, slik at vi kontinuerlig kan forbedre
+                </Primitives.Paragraphs.P>
+                <Primitives.Heading.H2>Kontakt oss</Primitives.Heading.H2>
+                <Primitives.Paragraphs.P>
+                Er det noen spørsmål du er usikker på underveis? Da kan du <a href="https://datalandsbyen.norge.no">stille spørsmål i Datalandsbyen</a> eller <a href="https://datalandsbyen.norge.no/category/9/juss-og-data">ta kontakt med Nasjonalt ressurssenter for deling og bruk av data</a>.
                 </Primitives.Paragraphs.P>
                 <br />
                 <Primitives.Button.MainButton type="button" onClick={() => close()}>
